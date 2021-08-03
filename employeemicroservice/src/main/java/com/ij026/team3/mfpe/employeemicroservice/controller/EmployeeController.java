33830aa78622fa53ff9baf6d3eb89e9858c392a6
gurusai <51899461+gurusai123@users.noreply.github.com>
2021-08-03 14:30:55 +0530

    Exception Handler

--- employeemicroservice/src/main/java/com/ij026/team3/mfpe/employeemicroservice/controller/EmployeeController.java
+++ employeemicroservice/src/main/java/com/ij026/team3/mfpe/employeemicroservice/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -129,4 +130,8 @@ public class EmployeeController {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 	}
 
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<String> handleAllException(RuntimeException run_exception) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(run_exception.getMessage());
+	}
 }
